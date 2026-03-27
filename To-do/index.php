58a01d3067ba67fb8a4e6372621860bc691b0a2d
Neeeschal <nischal0816@gmail.com>
2026-03-28 00:19:55 +0545

    Add files via upload

--- To-do/index.php
+++ To-do/index.php
@@ -1,1 +1,29 @@
+<?php include 'db.php'; ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task Manager</title>
+</head>
+<body>
+<h2>Task Manager</h2>
+
+<form method="POST" action="add.php">
+    <input type="text" name="task" placeholder="Enter task" required>
+    <button type="submit">Add Task</button>
+</form>
+
+<h3>Tasks</h3>
+<ul>
+<?php
+$result = $conn->query("SELECT * FROM tasks");
+while($row = $result->fetch_assoc()){
+    echo "<li>" . $row['task'] . " 
+    <a href='edit.php?id=".$row['id']."'>Edit</a> 
+    <a href='delete.php?id=".$row['id']."'>Delete</a></li>";
+}
+?>
+</ul>
+
+</body>
+</html>
